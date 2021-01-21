8811f8650c86216e6e062ef8422c53329d07fca9
hiroki1600 <hiroki9452@gmail.com>
2021-01-21 23:36:04 +0900

    コントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,24 @@ def index
   @items = Item.all.order("created_at DESC")
 end
 
+def new
+  @item = Item.new
+end
+
+def create
+  @item = Item.new(item_params)
+  if @item.valid?
+     @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :text, :price).merge(user_id: current_user.id)
+end
+
 end
