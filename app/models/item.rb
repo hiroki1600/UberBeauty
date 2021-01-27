00d9a24bf649b7d7e84adf7de66e81f952a1fbe6
hiroki1600 <hiroki9452@gmail.com>
2021-01-27 21:04:16 +0900

    投稿機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :text
     validates :price, format: { with: /\A[0-9]+\z/ }
-    validates :image
+    validates :images
   end
 
   has_many_attached :images
 
   validates_inclusion_of :price, in: 1000..5000
+
+  belongs_to :user
 
 end
