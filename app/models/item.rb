5e8e898877825986038e2b871ddea549e6e24150
hiroki1600 <hiroki9452@gmail.com>
2021-01-27 17:24:55 +0900

    レイアウト変更（タイトル追加）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
 
+  with_options presence: true do
+    validates :text
+    validates :price, format: { with: /\A[0-9]+\z/ }
+    validates :image
+  end
+
   has_many_attached :images
+
+  validates_inclusion_of :price, in: 1000..5000
 
 end
